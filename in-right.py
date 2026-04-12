cff89bc825c90aa90cab2746d4a0d3b190e4928d
AmoghSrivastav007 <158320428+AmoghSrivastav007@users.noreply.github.com>
2026-04-12 22:29:27 +0530

    Add files via upload

---
+++ in-right.py
@@ -0,0 +1,6 @@
+row = int(input("Enter the row size for the pattern:"))
+
+for i in range(row, 0, -1):      # Outer loop for rows in reverse
+    for j in range(1, i+1):      # Inner loop for columns
+        print("*", end = "")     # print star
+    print()
